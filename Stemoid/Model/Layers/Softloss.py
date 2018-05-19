2b643dc5744380b13af9371d39213ddf8f1f4276
sho <shojabon@gmail.com>
2018-05-19 18:29:36 +0900

    fixed softmaxwith loss function

--- Stemoid/Model/Layers/Softloss.py
+++ Stemoid/Model/Layers/Softloss.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-from functions import softmax, cross_entropy_error
 
 
 class SoftLoss:
@@ -9,10 +8,31 @@ class SoftLoss:
         self.y = None
         self.t = None
 
+    def cross_entropy_error(self, y, t):
+        if y.ndim == 1:
+            t = t.reshape(1, t.size)
+            y = y.reshape(1, y.size)
+
+        if t.size == y.size:
+            t = t.argmax(axis=1)
+
+        batch_size = y.shape[0]
+        return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+    def softmax(self, x):
+        if x.ndim == 2:
+            x = x.T
+            x = x - np.max(x, axis=0)
+            y = np.exp(x) / np.sum(np.exp(x), axis=0)
+            return y.T
+
+        x = x - np.max(x)
+        return np.exp(x) / np.sum(np.exp(x))
+
     def forward(self, x, t):
         self.t = t
-        self.y = softmax(x)
-        self.loss = cross_entropy_error(self.y, self.t)
+        self.y = self.softmax(x)
+        self.loss = self.cross_entropy_error(self.y, self.t)
 
         return self.loss
 
